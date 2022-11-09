357a6ebe9fbfced30dccc80b7fa2b5f1e1fd1393
ELEC_JUNG <69679055+hanminJ@users.noreply.github.com>
2022-11-09 15:25:03 +0900

    Add files via upload

---
+++ javascript 200제 풀이/part1/009.js
@@ -0,0 +1,11 @@
+var str = 'JavaScript';
+var num = 200;
+var arr = [1, 2, 3, 4, 5];
+var obj = {a: 1, b: 2, c: 3};
+
+console.log(str);
+console.log(num);
+console.log(arr);
+console.log(obj);
+
+/* debug -log와 동일 메세지 , warn - 경고성 메세지 ,error -에러매세지,info -정보성 메세지 */
